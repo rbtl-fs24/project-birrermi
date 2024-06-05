cf22733cf9c9a3d85f38f57441320850ac590bd9
birrermi <birrermi@student.ethz.ch>
2024-06-05 15:47:19 +0000

    processed_data

--- R/02-data_cleaning.R
+++ R/02-data_cleaning.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 
-survey_processesed <- survey_raw <- read.csv("data/raw/survey_raw.csv")
+#import data
+survey_processed <- survey_raw <- read.csv("data/raw/survey_raw.csv")
 
-survey_processesed <- survey_processesed |> 
+#rename columns
+survey_processed <- survey_processed |> 
   rename(purpose = On.what.purpose.are.you.regularly.visiting.the.ETH.Campus.,
          enrollment_status = What.is.your.enrollment.status.at.ETH..  ,
          department = In.which.department.do.you.study.. ,
@@ -27,5 +29,31 @@ survey_processesed <- survey_processesed |>
          favourite_spot_ETH_Hönggerberg = Where.is.your.favourite.most.used.recycling.spot.at.the.ETH.Hönggerberg.Campus..   ,
          improvement_ETH_Hönggerberg = Is.there.something.missing.or.to.be.improved.in.terms.of.the.given.recycling.infrastructure.....24,
          general_remarks = You.answered.all.necessary.questions..Thanks.for.your.time..and.participation..If.there.is.anything.you.want.to.adress.use.the.box.below.)
-         
+
+#delete general_remarks column         
+survey_processed <- survey_processed |> 
+  select(-general_remarks)
+
+#reorder table
+survey_processed <- survey_processed |> 
+  relocate(campus, .before = purpose)
+survey_processed <-  survey_processed |> 
+  arrange(campus,
+          purpose,
+          enrollment_status)
+
+#add ID
+survey_processed <-  survey_processed |> 
+  mutate(ID = seq(1:n())) 
+survey_processed <-  survey_processed |> 
+  relocate(ID, .before = campus)
+
+#ID 3 and 4 work as phd move to study as phd
+survey_processed$purpose[survey_processed$ID == 3] <- "Study"
+survey_processed$enrollment_status[survey_processed$ID == 3] <- "Phd"
+survey_processed$purpose[survey_processed$ID == 4] <- "Study"
+survey_processed$enrollment_status[survey_processed$ID == 4] <- "Phd"
+
+write_csv(survey_processed, "data/processed/survey_processed.csv" )
+
 
